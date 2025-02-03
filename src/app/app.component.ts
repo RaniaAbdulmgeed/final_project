49a0fcc103a9fa5a1336c855cc37190e1512d0a6
Rania <raniaabdelmgeed25@gmail.com>
2025-02-03 21:40:38 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'final_project';
+  showHeaderFooter = true;
+
+  constructor(private router: Router) {
+    // Watch the route and hide header/footer for specific routes
+    this.router.events.subscribe(() => {
+      const currentRoute = this.router.url;
+
+      // Define routes where you don't want the header and footer
+      const noHeaderFooterRoutes = ['/dashboard']; // Add more routes if needed
+
+      // Show or hide header/footer based on the current route
+      this.showHeaderFooter = !noHeaderFooterRoutes.includes(currentRoute);
+    });
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/homepage']); // Example of navigating to the homepage
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
